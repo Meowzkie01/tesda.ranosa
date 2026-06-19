04c6fee49f090985fc96a8a86602b9c9ea066646
Marwill Ranosa <drexxerity@gmail.com>
2026-06-19 22:09:05 +0000

    Task9 Done

---
+++ Task9.java
@@ -0,0 +1,38 @@
+import static java.lang.Math.*;
+import java.util.Scanner;
+
+public class Task9 {
+
+    public static int add(int a, int b) {
+        return addExact(a, b);
+    }
+
+    public static int subtract(int a, int b) {
+        return subtractExact(a, b);
+    }
+
+    public static int multiply(int a, int b) {
+        return multiplyExact(a, b);
+    }
+
+    public static int divide(int a, int b) {
+        return floorDiv(a, b); // integer division
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int y = sc.nextInt();
+
+        System.out.println("Addition: " + add(x, y));
+        System.out.println("Subtraction: " + subtract(x, y));
+        System.out.println("Multiplication: " + multiply(x, y));
+        System.out.println("Division: " + divide(x, y));
+
+        sc.close();
+    }
+}
